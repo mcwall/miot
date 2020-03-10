07742110ca7c26bb154cce983f80c8c8fb9ac186
Matt Wallick <mcwallick@gmail.com>
2020-03-10 02:48:06 -0400

    Basic light toggling via web

---
+++ www/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from rpi_rf import RFDevice
+
+app = Flask(__name__)
+
+pulselength = 355
+protocol = 1
+tx_gpio = 23
+on_code = 5768088
+off_code = 5768084
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/light')
+def light():
+    state = request.args.get('state')
+    on = state == 'true'
+    code = on_code if on else off_code
+    print('Turning ' + ('on' if on else 'off') + '...' )
+
+    rfdevice = RFDevice(tx_gpio)
+    rfdevice.enable_tx()
+    rfdevice.tx_code(code, protocol, pulselength)
+    rfdevice.cleanup()
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
